61220b4f7284eb48583ba93a0df0453019096dc9
Lucas Diniz <53830752+Akssasori@users.noreply.github.com>
2021-02-10 15:38:03 -0300

    logica if else

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,19 +2,38 @@ const User = require('../models/User');
 
 module.exports = {
   async store(req, res) { //async
-    return res.json(user);
 
     try {
 
       const { name, email } = req.body;
-      if (typeof name === "undefined" || typeof email === "undefined") return res.json({ error: "nome e/ou email undefined" });
-      return res.json({ sucesso: { name, email });
-    } cacth(err) {
-      if (err) console.log(err);
-      return res.json({ error: 1 });
-    };
+
+      if (typeof name === "undefined" || typeof email === "undefined") {
+        return res.json({ error: "nome e/ou email undefined" });
+
+      } else {
+
+        const user = await User.create({ name, email }); //aguarda a resposta do User.create;
+        if (user) { // verifica se o User.create ja respondeu;
+          if (user.name && user.email)
+            return res.json(user); // retorna com "user" se houverem name e email no response do User.create;
+
+          return res.json({ error: '"user.name"&&"user.email" == undefined' }); // retorna com a mensagem de erro, caso não tenha name e email no response do User.create;
+        }
+
+      }
+    }
+    catch(err){
+        if (err) {
+          console.log(err);
+          return res.json({ error: 1 });
+        }if(err){
+          console.log(err);
+          return res.json({ error: 2 });
+        }
+      }  
+      
+
+    
   }
 
-
-};
-
+}
