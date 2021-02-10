010cfd9e53c98a3de23fc06de10580dcf09f5af5
Lucas Diniz <53830752+Akssasori@users.noreply.github.com>
2021-02-10 14:41:18 -0300

    erro no try

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,27 +1,20 @@
 const User = require('../models/User');
 
 module.exports = {
-  
-    async store(req, res) { //async
-        const { name, email } = req.body;
-        console.log(req.body);
-    
-        const user = await User.create({ name, email }); //await
-        if(user){
-          if(user.name&&user.email)
-          return res.json(user);
-          return res.json({error:"user.name"&&"user.email" == undefined});
-        }
+  async store(req, res) { //async
+    return res.json(user);
 
-        // try {
-        //   return res.json(user);
-        // } catch (error) {
-        //   return error
-          
-        // }
-        
-    
-        // return res.json(user);
-      }
-      
+    try {
+
+      const { name, email } = req.body;
+      if (typeof name === "undefined" || typeof email === "undefined") return res.json({ error: "nome e/ou email undefined" });
+      return res.json({ sucesso: { name, email });
+    } cacth(err) {
+      if (err) console.log(err);
+      return res.json({ error: 1 });
     };
+  }
+
+
+};
+
